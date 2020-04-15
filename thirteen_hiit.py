9ea71b18752a416975b35281bfd9e95eadd8d62e
Sundae2608 <60578595+Sundae2608@users.noreply.github.com>
2020-04-14 23:10:13 -0700

    Thirteen HIIT

---
+++ thirteen_hiit.py
@@ -0,0 +1,17 @@
+from api import Exercise
+
+x = Exercise("ten_minutes_abs/")
+x.add_exercise("Hand Release Push up", 45, 15)
+x.add_exercise("Plyo Push-Ups", 45, 15)
+x.add_exercise("Russian Twists", 45, 15)
+x.add_exercise("Single-Leg Burpees", 45, 15)
+x.add_exercise("Supermans with Lateral Raises", 45, 15)
+x.add_exercise("Lateral Lunges with Hops", 45, 15)
+x.add_exercise("Tuck-Ups", 45, 15)
+x.add_exercise("Mountain Climbers", 45, 15)
+x.add_exercise("Plank Jacks", 45, 15)
+x.add_exercise("Squat Thrusts", 45, 15)
+x.add_exercise("Plank-Ups", 45, 15)
+x.add_exercise("Flutter Kicks", 45, 15)
+x.add_exercise("Star Jumpss", 45, 15)
+x.play_exercise()
